8c629b6d935b6bf8eda3a5eda86b88f5f1e7ef0b
Matthias Endler <matthias.endler@trivago.com>
2016-02-02 22:15:32 +0100

    Format example messages in json encoder

--- kafka_influxdb/encoder/collectd_json_encoder.py
+++ kafka_influxdb/encoder/collectd_json_encoder.py
@@ -15,7 +15,22 @@ class Encoder(object):
 
     Sample measurements:
 
-    [{"values":[0],"dstypes":["derive"],"dsnames":["value"],"time":1436372292.412,"interval":10.000,"host":"26f2fc918f50","plugin":"cpu","plugin_instance":"1","type":"cpu","type_instance":"interrupt"}]
+    [
+       {
+          "values":               [0],
+          "dstypes":       ["derive"],
+          "dsnames":        ["value"],
+          "time":      1436372292.412,
+          "interval":          10.000,
+          "host":      "26f2fc918f50",
+          "plugin":             "cpu",
+          "plugin_instance":      "1",
+          "type":               "cpu",
+          "type_instance": "interrupt"
+       }
+    ]
+
+    or
 
     [
        {
@@ -24,14 +39,15 @@ class Encoder(object):
          "dsnames":    ["value"],
          "time":      1280959128,
          "interval":          10,
-         "host":            "leeloo.octo.it",
-         "plugin":          "cpu",
-         "plugin_instance": "0",
-         "type":            "cpu",
-         "type_instance":   "idle"
+         "host":"leeloo.octo.it",
+         "plugin":         "cpu",
+         "plugin_instance":  "0",
+         "type":           "cpu",
+         "type_instance":  "idle"
        }
     ]
     """
+
     def encode(self, msg):
         measurements = []
 
