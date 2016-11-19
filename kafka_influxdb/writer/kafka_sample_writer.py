6a9437fb638b44741d6668fa274b8cb720c5e8e9
Matthias Endler <matthias.endler@trivago.com>
2016-11-19 16:55:23 +0100

    WIP fix Kafka sample message writer

--- kafka_influxdb/writer/kafka_sample_writer.py
+++ kafka_influxdb/writer/kafka_sample_writer.py
@@ -1,8 +1,6 @@
-import kafka
-import kafka.common
+from kafka import KafkaProducer
 import random
 import logging
-import time
 
 
 class KafkaWriterException(Exception):
@@ -16,7 +14,8 @@ class KafkaSampleWriter(object):
     """
 
     def __init__(self, host, port, topic):
-        self.kafka_client = self._create_kafka_client(host, port)
+        logging.info("Connecting to Kafka broker at %s:%s", host, port)
+        self.kafka_client = KafkaProducer(bootstrap_servers="{}:{}".format(host, port))
         self.topic = topic
 
         self.sample_messages = [
@@ -27,48 +26,17 @@ class KafkaSampleWriter(object):
             b"26f2fc918f50.memory.memory-buffered 743657472 1436357630"
         ]
 
-    def produce_messages(self, batches=1000, batch_size=1000):
+    def produce_messages(self, num_messages=1000000):
         """
         Produce Kafka sample messages
         :param batches: number of message batches
         :param batch_size: messages per batch
         :return:
         """
-        partitions = self._get_partitions(self.topic)
-        if not partitions:
-            raise KafkaWriterException("No partitions found for %s" % self.topic)
-
-        kafka_messages = self._create_random_messages(self.sample_messages, batch_size)
-        kafka_requests = [self._create_request(self.topic, p, kafka_messages) for p in partitions]
-        self._send_request_batches(kafka_requests, batches, batch_size)
-
-    def _send_request_batches(self, kafka_requests, batches, batch_size):
-        total_messages = batches * batch_size
-        for i in range(batches):
-            self._send_requests(kafka_requests)
-            logging.info("Sent %s out of %s messages", i * batch_size, total_messages)
-        self.kafka_client.close()
-
-    def _send_requests(self, requests):
-        try:
-            self.kafka_client.send_produce_request(payloads=requests, fail_on_error=True)
-        except kafka.common.UnknownTopicOrPartitionError as e:
-            logging.error(e)
-            time.sleep(1)
-            self.kafka_client.close()
-
-    def _get_partitions(self, topic):
-        return self.kafka_client.get_partition_ids_for_topic(topic)
-
-    @staticmethod
-    def _create_kafka_client(host, port):
-        logging.info("Connecting to Kafka broker at %s:%s", host, port)
-        return kafka.KafkaClient("{}:{}".format(host, port))
-
-    @staticmethod
-    def _create_random_messages(messages, count):
-        return [kafka.create_message(random.choice(messages)) for _ in range(count)]
-
-    @staticmethod
-    def _create_request(topic, partition, messages):
-        return kafka.common.ProduceRequest(topic=topic, partition=partition, messages=messages)
+        for i, message in enumerate(random.choice(self.sample_messages)):
+            if i > num_messages:
+                break
+            print(message)
+            result = self.kafka_client.send(self.topic, message)
+            print(result)
+        self.kafka_client.flush()
